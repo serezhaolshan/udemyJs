c9cd92b67b2cd6d640f5ea5878e98a3cfcdd4fdf
Sergey <olshan228@gmail.com>
2020-05-19 23:27:32 +0200

    Communication edit

--- простое общение с пользвоателем/script.js
+++ простое общение с пользвоателем/script.js
@@ -3,8 +3,8 @@
 
 //alert('Hello');
 
-//const result = confirm("Are you here?");
-//console.log(result);
+const result = confirm("Are you here?");
+console.log(result);
 
 // const answer = +prompt("Вам есть 18?","");
 // console.log(answer + 5);
